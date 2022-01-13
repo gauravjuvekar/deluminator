072d490b0c6f5518f403f271962a832fd608865a
Gaurav Juvekar <gauravjuvekar@gmail.com>
2022-01-13 04:03:35 -0800

    Updated Mint-Y includes switchable themes

--- config/dark.d/wm.sh
+++ config/dark.d/wm.sh
@@ -3,6 +3,6 @@
 # Copyright 2021, Gaurav Juvekar
 # SPDX-License-Identifier: MIT
 
-gsettings set org.gnome.desktop.wm.preferences    theme "Mint-Y-Dark"
-gsettings set org.cinnamon.desktop.wm.preferences theme "Mint-Y-Dark"
+gsettings set org.gnome.desktop.wm.preferences    theme "Mint-Y"
+gsettings set org.cinnamon.desktop.wm.preferences theme "Mint-Y"
 
