4faf3e1f03035d7a76eeb9f45455b11ec3c2e0ec
Gaurav Juvekar <gauravjuvekar@gmail.com>
2022-08-04 02:17:51 -0700

    Use color-scheme for gtk4

--- config/dark.d/gtk.sh
+++ config/dark.d/gtk.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Copyright 2021, Gaurav Juvekar
+# Copyright 2022, Gaurav Juvekar
 # SPDX-License-Identifier: MIT
 
-gsettings set org.gnome.desktop.interface    gtk-theme "Mint-Y-Dark"
-gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y-Dark"
+gsettings set org.gnome.desktop.interface    gtk-theme    "Mint-Y-Dark"
+gsettings set org.gnome.desktop.interface    color-scheme "prefer-dark"
+gsettings set org.cinnamon.desktop.interface gtk-theme    "Mint-Y-Dark"
--- config/light.d/gtk.sh
+++ config/light.d/gtk.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Copyright 2021, Gaurav Juvekar
+# Copyright 2022, Gaurav Juvekar
 # SPDX-License-Identifier: MIT
 
-gsettings set org.gnome.desktop.interface    gtk-theme "Mint-X"
-gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-X"
+gsettings set org.gnome.desktop.interface    gtk-theme    "Mint-X"
+gsettings set org.gnome.desktop.interface    color-scheme "prefer-light"
+gsettings set org.cinnamon.desktop.interface gtk-theme    "Mint-X"
